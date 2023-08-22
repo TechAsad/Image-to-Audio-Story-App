12f3945a591242d2979a3d781c23fe95427a9306
Muhammad Asadullah <100842006+TechAsad@users.noreply.github.com>
2023-08-23 02:22:37 +0500

    Update txt2img.py

--- txt2img.py
+++ txt2img.py
@@ -2,15 +2,32 @@ import requests
 import streamlit as st
 from PIL import Image
 import io
+import PIL
 
 API_URL = "https://api-inference.huggingface.co/models/LinoyTsaban/lora-xl-3d_icons-0.0001-5e-05-1500-1-None"
 headers = {"Authorization": "Bearer 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"}
 
 def generate_image(prompt):
     response = requests.post(API_URL, headers=headers, json={"inputs": prompt})
-    image_bytes = response.content
-    image = Image.open(io.BytesIO(image_bytes))
-    return image
+    
+    if response.status_code == 200:
+        image_bytes = response.content
+        
+        try:
+            image = Image.open(io.BytesIO(image_bytes))
+            return image
+        except PIL.UnidentifiedImageError as e:
+            st.error("Error: Unable to identify the generated image.")
+            st.text(f"UnidentifiedImageError: {e}")
+            st.text("Image Bytes:")
+            st.text(image_bytes)
+            return None
+    else:
+        st.error("Error: Unable to generate image.")
+        st.text(f"API Response Status Code: {response.status_code}")
+        st.text("API Response Content:")
+        st.text(response.content)
+        return None
 
 def main():
     st.set_page_config(page_title="Prompt to Image Generator", page_icon="🌄")
@@ -20,8 +37,9 @@ def main():
 
     if st.button("Generate Image"):
         image = generate_image(prompt)
-        st.subheader("Generated Image:")
-        st.image(image, caption="Generated Image", use_column_width=True)
+        if image is not None:
+            st.subheader("Generated Image:")
+            st.image(image, caption="Generated Image", use_column_width=True)
 
 if __name__ == "__main__":
     main()
