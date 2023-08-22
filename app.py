2a76cf29600e2a62c692b42f0e3f3bce409d14b5
Muhammad Asadullah <100842006+TechAsad@users.noreply.github.com>
2023-08-22 07:50:27 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,62 +1,55 @@
 from transformers import pipeline
 from langchain import PromptTemplate, LLMChain
 from langchain.llms import GooglePalm
-import torch
+
 import requests
 import os
 import streamlit as st
 
-
 os.environ["GOOGLE_API_KEY"] = "AIzaSyD29fEos3V6S2L-AGSQgNu03GqZEIgJads"
-os.environ ["HUGGINGFACEHUB_API_TOKEN"] = "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"
-
-llm = GooglePalm(temperature = 0.7)
 
 
-#image to text
+llm = GooglePalm(temperature=0.7)
 
-def image2text(url):
-    image_to_text = pipeline("image-to-text", model = "Salesforce/blip-image-captioning-large")
-
-    text = image_to_text(
-        url)[0]['generated_text']
+# Image to text using Hugging Face API
+def image2text(filename):
+    API_URL1 = "https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-large"
+    headers1 = {"Authorization": "Bearer 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"}
     
-    print(text)
-    return(text)
+    with open(filename, "rb") as f:
+        data = f.read()
+        
+    response = requests.post(API_URL1, headers=headers1, data=data)
+    response_data = response.json()
+    
+    if "generated_text" in response_data[0]:
+        generated_text = response_data[0]["generated_text"]
+        return generated_text
+    else:
+        return "Image caption not available."
 
-#story teller
 def generate_story(scenario):
-    template = """"
+    template = """
     You are a story teller;
     you can generate a creative fun story based on a sample narrative, the story should not be more than 100 words;
 
     CONTEXT: {scenario}
     STORY: 
     """
-
-    prompt = PromptTemplate(template = template, 
-        input_variables = ['scenario']
-    )
-    story_llm = LLMChain(llm=llm, prompt = prompt, verbose = True)
-
-    story = story_llm.predict(scenario = scenario)
-
-    print(story)
-    return(story)
-
-#text to speech
+    prompt = PromptTemplate(template=template, input_variables=['scenario'])
+    story_llm = LLMChain(llm=llm, prompt=prompt, verbose=True)
+    story = story_llm.predict(scenario=scenario)
+    return story
 
 def text2speech(message):
-     API_URL = "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
-     headers = {"Authorization": "Bearer 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"}
-     payloads = {
-          "inputs":message
-     }
-     response = requests.post(API_URL, headers = headers, json= payloads)
-     with open("audio.flac", "wb") as file:
-         file.write(response.content)
-
-
+    API_URL = "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
+    headers = {"Authorization": "Bearer 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"}
+    payloads = {
+        "inputs": message
+    }
+    response = requests.post(API_URL, headers=headers, json=payloads)
+    with open("audio.flac", "wb") as file:
+        file.write(response.content)
 
 def main():
     st.set_page_config(page_title="Your Image to Audio Story", page_icon="🦜")
@@ -68,8 +61,7 @@ def main():
         bytes_data = uploaded_file.getvalue()
         with open(uploaded_file.name, 'wb') as file:
             file.write(bytes_data)
-        st.image(uploaded_file, caption="Uploaded Image",
-                 use_column_width= True)
+        st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
         
         scenario = image2text(uploaded_file.name)
         st.subheader("Image Details:")
@@ -87,7 +79,6 @@ def main():
         st.subheader("Download Audio:")
         with open("audio.flac", "rb") as audio_file:
             st.download_button(label="Download Audio", data=audio_file, file_name="generated_audio.flac", mime="audio/flac")
-
 
 if __name__ == "__main__":
     main()
